7b99418e438948797ba3fffba86e2f39864009ae
jglrxavpok <jglrxavpok@gmail.com>
2026-05-31 23:27:45 +0200

    Remove a few remaining GLSL GBuffer shader code

--- engine/resources/shaders/includes/viewport.glsl
+++
@@ -1,8 +0,0 @@
-#define DEFINE_VIEWPORT_CONSTANT(setID)                                                                                 \
-layout(set = setID, binding = 0) uniform ViewportConstant {                                                             \
-    uint frameCount;                                                                                                    \
-    uint frameWidth;                                                                                                    \
-    uint frameHeight;                                                                                                   \
-                                                                                                                        \
-    bool hasTLAS;                                                                                                       \
-} viewport;
